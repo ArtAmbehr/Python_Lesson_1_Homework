0964228875107981107071d794b9177ed1d8f1cb
Art <art-75@yandex.ru>
2023-01-14 23:38:46 +0300

    Task.40

---
+++ HW_5/Task_40.py
@@ -0,0 +1,23 @@
+# 40.	Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+def encode(s):
+
+	encoding = ""
+
+	i = 0
+	while i < len(s):
+		count = 1
+
+		while i + 1 < len(s) and s[i] == s[i + 1]:
+			count = count + 1
+			i = i + 1
+
+		encoding += str(count) + s[i]
+		i = i + 1
+
+	return encoding
+
+if __name__ == '__main__':
+
+	s = '111112222334445'
+	print(encode(s))
